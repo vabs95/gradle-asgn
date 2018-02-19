9501be2cfc6a5ec29fb042f15e4ebc3ee7884e93
vaibhav <sethvaibhav95@gmail.com>
2018-02-19 11:08:56 +0530

    First Commit

---
+++ src/main/java/com/ttn/asgn/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.ttn.asgn;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        bye b = new bye();
+        b.display();
+    }
+}
---
+++ src/main/java2/com/ttn/asgn/bye.java
@@ -0,0 +1,7 @@
+package com.ttn.asgn;
+public class bye {
+    void display()
+    {
+        System.out.println("Bye");
+    }
+}
