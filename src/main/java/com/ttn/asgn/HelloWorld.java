c4f935205caaafcb7a5977bcfd38f8cf9a86cb7b
vaibhav <sethvaibhav95@gmail.com>
2018-02-21 09:44:25 +0530

    final

--- src/main/java/com/ttn/asgn/HelloWorld.java
+++ src/main/java/com/ttn/asgn/HelloWorld.java
@@ -1,9 +1,16 @@
 package com.ttn.asgn;
+import org.apache.commons.lang3.StringUtils; //Q4
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         bye b = new bye();
         b.display();
+
+        String n="Vaibhav";
+        String n1=StringUtils.upperCase(n);
+        System.out.println(n1);
+
     }
 }
+
